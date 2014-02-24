8062755a35e9cdb5f1dceb52162b740ccdc1fc94
Jason Liu <jhonedlergg@gmail.com>
2014-02-24 22:34:49 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Printeresting::Application.config.secret_key_base = 'c1d2be65dc082581f99d5599c3ed16879d981a1e86fc4abd1be9ec58bfc095c39befbe12cfec706960f771deb1dd2f33ee6bc824574976138ff8bd4c8b346f5a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Printeresting::Application.config.session_store :cookie_store, key: '_printeresting_session'
